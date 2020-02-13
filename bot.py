f216731cc36ef3b5dcc9a585be4ec1345710d851
BryanWhitehurst <bjwhitehurst@crimson.ua.edu>
2020-02-13 15:36:25 -0600

    /

---
+++ bot.py
@@ -0,0 +1,16 @@
+from generatetext import getMsg
+import tweepy, time
+from credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+i = 0
+while True:
+    api.update_status(getMsg())
+    time.sleep(1800)
+    i = i+ 1
---
+++ generatetext.py
@@ -0,0 +1,20 @@
+import requests, json
+
+
+
+def getMsg():
+    api_key = "6d1bb05fca4a5354af4b04f27a3f0479"
+    url = "http://api.openweathermap.org/data/2.5/weather?appid=6d1bb05fca4a5354af4b04f27a3f0479&q=Tuscaloosa"
+    response = requests.get(url)
+
+    x = response.json()
+
+    y = x["main"]
+    current_temp = y["temp"]
+    current_temp = (current_temp - 273.15)*(9/5) + 32
+    z = x["weather"]
+    weather_desc = z[0]["description"]
+
+    output = "It is currently " + str(round(current_temp, 0)) + " degrees fahrenheit in Tuscaloosa, AL with " + weather_desc
+    return output 
+
