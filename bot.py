f7ad0a44fe53cb33ff0b569054da171205dc52f1
BryanWhitehurst <bjwhitehurst@crimson.ua.edu>
2020-02-13 21:50:42 -0600

    tweet will only go out on the hour

--- bot.py
+++ bot.py
@@ -9,6 +9,12 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
 api = tweepy.API(auth)
 
+while True:
+    result = time.localtime()
+    mins = result.tm_min
+    if mins == 0:
+        break
+
 i = 0
 while True:
     api.update_status(getMsg())
